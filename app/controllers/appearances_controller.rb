db42b93595da5dfe46dfa101435b3225237ccbcb
wilsonng21 <wilsongch92@gmail.com>
2020-08-31 09:41:05 -0400

    show pages work for guest and episode

--- app/controllers/appearances_controller.rb
+++ app/controllers/appearances_controller.rb
@@ -5,7 +5,13 @@ class AppearancesController < ApplicationController
     end
 
     def create 
-        @appearance = Appearance.create(appearance_params)
+        @appearance = Appearance.new(appearance_params)
+        if @appearance.save 
+            # byebug
+            redirect_to episode_path(@appearance.episode_id) #selected episode's show page
+        else 
+            redirect_to new_appearance_path
+        end
     end
 
     private 
