6199053bc0040e3abf4f905c8451767f082abb4d
wilsonng21 <wilsongch92@gmail.com>
2020-08-30 22:16:17 -0400

    form able to create new appearance instance

---
+++ app/controllers/appearances_controller.rb
@@ -0,0 +1,16 @@
+class AppearancesController < ApplicationController
+
+    def new 
+        @appearance = Appearance.new
+    end
+
+    def create 
+        @appearance = Appearance.create(appearance_params)
+    end
+
+    private 
+
+    def appearance_params
+        params.require(:appearance).permit(:guest_id, :episode_id, :rating)
+    end
+end
