c6dde79167b608399d8fb3e8300ef5fa681ddc02
Dong-Yi-Xia <dong7777xia@gmail.com>
2020-08-31 17:06:03 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  resources :appearances
+  # resources :appearances
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :guests, only: [:index]
-  resources :episodes, only: [:index]
+  resources :guests, only: [:index, :show]
+  resources :episodes, only: [:index, :show]
+
+  get "/appearances", to: "appearances#index", as: :appearances
+  get "/appearances/new", to: "appearances#new", as: :new_appearance
+  post "/appearances", to: "appearances#create"
+  get "/appearances/:id", to: "appearances#show", as: :appearance
+  get "/appearances/:id/edit", to: "appearances#edit", as: :edit_appearance
+  patch "/appearances/:id", to: "appearances#update"
+  delete "appearances/:id", to: "appearances#destroy"
+
+
 end
