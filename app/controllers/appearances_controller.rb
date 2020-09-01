f0fad99b69650cf7000c275a77842d52c460b6f3
iannakim <dear.hyunji@gmail.com>
2020-09-01 18:53:57 -0400

    appearances index and new created

--- app/controllers/appearances_controller.rb
+++ app/controllers/appearances_controller.rb
@@ -1,2 +1,35 @@
 class AppearancesController < ApplicationController
+
+    
+    def index
+        @appearances = Appearance.all
+        @guests = Guest.all 
+        @episodes = Episode.all
+    end
+
+    
+    def new
+        @appearance = Appearance.new
+        @guests = Guest.all 
+        @episodes = Episode.all
+    end
+    
+    def create
+        episode = Episode.find(params[:id])
+        @appearance = Appearance.create(appearance_params)
+        # if @appearance.valid?
+            redirect_to episode_path(episode)
+        #   else 
+        #     flash[:errors] = @appearance.errors.full_messages
+        #     redirect_to new_appearance_path
+        #   end
+    end
+
+private
+
+    def appearance_params
+        params.require(:appearance).permit(:rating, :guest_id, :episode_id)
+    end
+
+
 end
