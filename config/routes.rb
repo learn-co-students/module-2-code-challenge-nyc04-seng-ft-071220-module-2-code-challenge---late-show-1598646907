d7d10a8509100dd3e5e1dae8090b41002235a60d
Elronia <zar.catherine@gmail.com>
2020-09-01 14:46:23 -0400

    setting associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :guests, only: [:index]
-  resources :episodes, only: [:index]
+  # resources :guests, only: [:index]
+  # resources :episodes, only: [:index]
+  get "/guests", to: "guests#index", as: "guests"
+  
+  get "/episodes", to: "episodes#index", as: "episodes"
+
+  get "/appearances/new", to: "appearances#new", as: "new_appearance"
 end
