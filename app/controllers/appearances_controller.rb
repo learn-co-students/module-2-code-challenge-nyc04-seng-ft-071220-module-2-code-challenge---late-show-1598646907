8eac99e5444990e8b296eb9f66e75bb9e4eba7f5
mirsafirst47 <habib.triande@gmail.com>
2020-09-01 18:48:57 -0400

    New appearance form

--- app/controllers/appearances_controller.rb
+++ app/controllers/appearances_controller.rb
@@ -1,2 +1,29 @@
 class AppearancesController < ApplicationController
+
+    # def index
+    #     @appearances = Appearance.all
+    # end
+
+    def new
+        @appearance = Appearance.new
+    end
+
+    def create
+        @appearance = Appearance.create(appearance_params)
+        redirect_to appearance_path(@appearance)
+    end
+
+    # def show
+    # end
+
+    # def edit
+    # end
+
+    # def update
+    # end
+
+    def appearance_params
+        params.require(:appearance).permit(:guest_id, :episode_id, :rating)
+    end
+
 end
