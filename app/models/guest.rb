505288f548a1fc9694840d668a75290dded645f9
Dong-Yi-Xia <dong7777xia@gmail.com>
2020-09-01 16:37:26 -0400

    rating order

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,10 +1,10 @@
 class Guest < ApplicationRecord
-    has_many :appearances
+    has_many :appearances, -> { order("rating DESC")}
     has_many :episodes, through: :appearances
     default_scope {order(name: :asc)}
 
 
     #dependent: :destroy ? resetting db:seed. Issue with dependent foreign Key.
 
-    
+
 end
