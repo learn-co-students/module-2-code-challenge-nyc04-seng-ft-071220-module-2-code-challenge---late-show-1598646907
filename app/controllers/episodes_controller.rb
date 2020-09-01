ffa14b051d22855fb663c6434c14c2279348c1db
sbalgobin94 <sbalgobin94@gmail.com>
2020-09-01 16:43:22 -0400

    first validation

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,6 +1,18 @@
 class EpisodesController < ApplicationController
+  
+  before_action :get_episode, only: [:show]
 
   def index
     @episodes = Episode.all
   end
+
+  def show
+  end
+
+  private
+
+    def get_episode
+        @episode = Episode.find(params[:id])
+    end
+
 end
--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,6 +1,18 @@
 class GuestsController < ApplicationController
 
+  before_action :get_guest, only: [:show]
+
   def index
     @guests = Guest.all
   end
+
+  def show
+  end
+
+  private
+
+    def get_guest
+      @guest = Guest.find(params[:id])
+    end
+
 end
