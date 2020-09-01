4466e3a5088d5723c6c13634508b343cc31701c4
wlcreate <waverley.nyc@gmail.com>
2020-09-01 17:55:59 -0400

    finishes Advanced Deliverable 6

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -3,4 +3,6 @@ class Appearance < ApplicationRecord
   belongs_to :episode
 
   validates :rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5, message: "is only from 1 to 5 (inclusive)"}
+  validates :guest_id, uniqueness: { scope: :episode_id, message: "is already recorded for this episode; A guest can only appear on an episode once" }
+  
 end
