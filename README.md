af920ea66b9dadf8e5727a5720ff4f9c51dc6d87
Nunny R <nunnyreyes@gmail.com>
2020-09-02 08:58:54 -0400

    creating a form

--- README.md
+++ README.md
@@ -78,14 +78,14 @@ To log that a specific Guest appeared on a certain Episode, we need to create th
 
 #this sounds like a validation
 
-### 2. New Appearance Form
+<!-- ### 2. New Appearance Form
 
 A user can fill out a form to create a new Appearance. They can:
 
 - Choose an existing guest from a select dropdown
 - Choose an existing episode from a select dropdown
 - Enter a numeric rating
-- Submit the form
+- Submit the form -->
 
 After submitting the form, the user should be redirected to the selected episode's show page.
 
--- app/controllers/appearances_controller.rb
+++ app/controllers/appearances_controller.rb
@@ -1,5 +1,7 @@
 class AppearancesController < ApplicationController
-   #I don't need to have an index here because I am focusing on 
+    ######I created a domain model its under assets > images ############
+   
+    #I don't need to have an index here because I am focusing on 
    #new and create. However I do need to have an index route
 
 
@@ -13,9 +15,33 @@ class AppearancesController < ApplicationController
     end
 
     def create
-        
+        #Now we want to save this instance to our db.
+        #We want to create it
+        #Going to create it using the private helper method
+        @appearance = Appearance.create(appearances_params)
+        #now include validations
+
+      
+        if @appearance.valid?
+            #if it exists then show me the episode?
+            #At the moment this is not redirecting me. 
+            #Also my form says to "create an appearance" but I have it 
+            #labeled as "submit" 🧐
+            redirect_to @appearance.episode
+        else
+            redirect_to new_appearance_path
+        end
     end
+        
+        
+   
 
     #I want to create a private method where I can save my params
     #Also for readability
+    private
+
+    def appearances_params
+        params.require(:appearance).permit(:episode_id, :guest_id)
+    end
+
 end
