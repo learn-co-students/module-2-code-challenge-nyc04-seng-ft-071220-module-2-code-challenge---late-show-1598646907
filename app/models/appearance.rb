5b77b80b2a4cee8ec00df95d0aac310cc8a8ad71
iannakim <dear.hyunji@gmail.com>
2020-09-02 09:52:20 -0400

    fix validate for guest appearnce

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -4,4 +4,12 @@ class Appearance < ApplicationRecord
 
   validates :rating, numericality: { only_integer: true }, inclusion: {in: 1..5, message: "needs to be between 1 and 5" } 
 
+  def appear_only_once
+    if #same guest and #episode already exists in appearance.all,
+      # throw an error
+      errors.add(:message, "A guest should only appear on an episode once!")
+    end
+  end
+
+
 end
