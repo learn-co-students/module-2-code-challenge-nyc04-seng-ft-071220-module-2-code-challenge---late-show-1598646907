71650b6600c1632166a511dada3773d28a2c70b9
mirsafirst47 <habib.triande@gmail.com>
2020-09-02 09:46:59 -0400

    Done

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -1,10 +1,10 @@
 class Appearance < ApplicationRecord
     belongs_to :guest
     belongs_to :episode
-
-    validates :rating, numericality: {only_integer: true}, inclusion: {in: 1..5, message: " from 1 through 5 "}
+    #need more practice with validations.
+    validates :rating, numericality: {only_integer: true}, inclusion: {in: 1..5, message: " must be from 1 through 5 "}
 
     #deliverable number 6
-    validates :guest_id, uniqueness: true
+    validates :guest_id, uniqueness: { scope: :episode_id, message: "appeared already for this episode"}
 
 end
