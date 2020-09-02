dd5662f72455135c31db2e213b44ba3be38f8902
BellaEvalle <bellaevalle@yahoo.com>
2020-09-02 09:17:39 -0400

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :guests, only: [:index]
   resources :episodes, only: [:index]
-end
+
+  get '/appearances', to: "appearances#new", as: "new_appearance"
+  get '/appearances/new', to: "appearances#new", as: "new_appearance" 
+  post '/appearances', to: "appearances#create" 
+
+  get '/episodes', to: "episodes#index", as: "episodes" 
+  get '/episodes/:id', to: "episodes#show", as: "episode" 
+
+  get '/guests', to: "guests#index", as: "guests" 
+  get '/guests/:id', to: "guests#show", as: "guest" 
+
+
