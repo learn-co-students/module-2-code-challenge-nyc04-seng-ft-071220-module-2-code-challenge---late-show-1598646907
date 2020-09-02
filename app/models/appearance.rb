8bb07b5c84db38bdb933c7ce644ca00fc7e32bf2
ksolomon7 <kalkidan.solomon2@gmail.com>
2020-09-01 23:39:24 -0400

    adds final deliverable

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -4,4 +4,6 @@ class Appearance < ApplicationRecord
   validates :rating, :numericality => {:greater_than_or_equal_to=>1, :less_than_or_equal_to=>5, :only_integer=> true} 
   # validates :rating, :inclusion=>{in: 1..5}
   validates :guest, :uniqueness => {scope: :episode, message: "already exists for that episode"}
+
+  default_scope {order(rating: :DESC)}
 end
